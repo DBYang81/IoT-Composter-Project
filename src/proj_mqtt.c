e110b1b3213d935a053591f74599284ee2d10737
jjuhaa <119441568+jjuhaa@users.noreply.github.com>
2022-12-12 12:28:37 +0200

    Update proj_mqtt.c

--- src/proj_mqtt.c
+++ src/proj_mqtt.c
@@ -28,7 +28,7 @@ void mqtt_connect_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t 
 
         mqtt_set_inpub_callback(client, mqtt_incoming_publish_cb, mqtt_incoming_data_cb, arg);
 
-        err = mqtt_subscribe(client, "cmd", 1, mqtt_sub_request_cb, arg);
+        err = mqtt_subscribe(client, "controller/command", 1, mqtt_sub_request_cb, arg);
 
         if (err != ERR_OK) {
             printf("mqtt_connect_cb: subscribe returns %d\n", err);
@@ -50,7 +50,8 @@ void mqtt_sub_request_cb(void *arg, err_t result) {
 void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
     printf("Incoming publish at topic %s with total length %u\n", topic, (unsigned int)tot_len);
 
-    if (strcmp(topic, "cmd") == 0) {
+    //TODO: modify
+    if (strcmp(topic, "controller/command") == 0) {
         inpub_id = 1;
     }
     else {
@@ -62,6 +63,7 @@ void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len) {
 void mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags) {
     printf("Incoming publish payload with length %d, flags %u\n", len, (unsigned int)flags);
 
+    //TODO: modify
     for (int i=0;i<len;i++) {
         printf("%c",data[i]);
     }
